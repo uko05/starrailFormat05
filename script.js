e89d87e91c43ed5b783d34e354c45d095a093fff
uko05 <yosieyosie1@yahoo.co.jp>
2025-06-17 19:53:33 +0900

    Move index.html to the root directory for GitHub Pages

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const imageFolder = 'chara/';
 const imageData = [
+    { src: 'seirensu.png', category: 'butsuri' },
     { src: 'fainon.png', category: 'butsuri' },
     { src: 'syu1.png', category: 'butsuri' },
     { src: 'sei1.png', category: 'butsuri' },
